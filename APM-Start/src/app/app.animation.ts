2bbc523cf5411709b4db44d15cc61fdfd51db0df
danielenyata <dkokyere@st.ug.edu.gh>
2021-11-05 14:11:06 +0000

    added animations

--- APM-Start/src/app/app.animation.ts
+++ APM-Start/src/app/app.animation.ts
@@ -14,8 +14,8 @@ export const slideInAnimation = trigger('slideInAnimation', [
         animate('0.5s ease-out', style({ transform: 'translateX(0%)' }))
       ], { optional: true }),
       query(':leave', [
-        style({ transform: 'translateX(0%)' }),
-        animate('0.5s ease-out', style({ transform: 'translateX(-100%)' }))
+        style({ transform: 'translateY(0%)' }),
+        animate('0.5s ease-out', style({ transform: 'translateY(100%)' }))
       ], { optional: true })
     ])
   ])
